8e8b5449501bd40087608b457e89df5d191ed0b8
Zakia Afrin Jeme <zjeme@techlaunch.io>
2019-02-14 02:47:40 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# New-Weather-Plugin
---
+++ weather.php
@@ -0,0 +1,54 @@
+<div class="container">
+	
+	<div id="icon-options-general"></div>
+	<h1 class="text">New Weather Plugin</h1>
+	
+	<div id="poststuff">
+		<div id="post-body-content">
+		<div id="post-body" class="metabox-holder columns-2">
+		
+			<!-- main content -->
+				
+				<div class="meta-box-sortables ui-sortable">
+
+
+					<div class="postbox">
+					
+						<h2><span>Enter Your Current Location :</span></h3>
+						<div class="inside">
+							
+							<form name="weather_form" method="post" action="">							
+
+							<input type="hidden" name="form_submitted" value="Y">
+
+							<table class="form-table">
+								<tr>
+									<td>
+										<label for="city">City / Zipcode </label>
+									</td><br/>
+									<td>
+										<input name="city" id="city" type="text" value="" class="regular-text" />
+									</td>
+								</tr>								
+							</table>
+
+							<p>
+								<input class="button-primary" type="submit" name="submit" value="Submit" /> 
+							</p>
+
+							</form>
+
+						</div> <!-- .inside -->
+					
+					</div> <!-- .postbox -->				
+
+				</div> <!-- .meta-box-sortables .ui-sortable -->
+				
+			</div> <!-- post-body-content -->
+			
+		</div> <!-- #post-body .metabox-holder .columns-2 -->
+		
+		<br class="clear">
+	</div> <!-- #poststuff -->
+	
+</div> <!-- .container -->
